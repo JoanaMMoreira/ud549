471f96e61df53d8e31aeee9794d196faae051de3
Joana Moreira <joanamoreira989@gmail.com>
2018-05-14 13:43:26 +0100

    Added test suites

--- spec/AddressBookSpec.js
+++ spec/AddressBookSpec.js
@@ -5,4 +5,11 @@ describe('Address Book', function () {
         addressBook.addContact(thisContact);
         expect(addressBook.getContact(0)).toBe(thisContact);
     });
+    it('should be able to delete a contact', fucntion() {
+        const addressBook = new AddressBook();
+        thisContact = new Contact();
+        addressBook.addContact(thisContact);
+        addressBook.deleteContact(0);
+        expect(addressBook.getContact(0)).not.toBeDefined();
+    });
 });
